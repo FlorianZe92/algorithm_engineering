21875ff00ca32753e204e743fa221877f93fcde3
FlorianZe92 <zeidler.florian@yahoo.de>
2022-03-01 21:18:32 +0100

    Exam Assignments

--- project/seed_data/dataset_converter.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt
-import pandas as pd
-
-
-read_file = pd.read_csv('angled_blob.csv',header=None)
-
-read_file[0].to_csv('x_Datasaurus', sep='\t', encoding='utf-8', header=False,index=False)
